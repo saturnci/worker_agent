277b1b39813509ea7b7d7e2bea5a01eb4be78982
Jason Swett <jason@benfranklinlabs.com>
2025-11-30 21:29:32 -0500

    Revert "Update FileContentRequest to use Request internally."
    This reverts commit 6c3a58e963d3342e780366b69ef48461dc64281f.

--- lib/saturn_ci_worker_api/file_content_request.rb
+++ lib/saturn_ci_worker_api/file_content_request.rb
@@ -1,7 +1,10 @@
-require_relative "request"
+require "net/http"
+require "uri"
+require_relative "api_config"
 
 module SaturnCIWorkerAPI
   class FileContentRequest
+    include APIConfig
     def initialize(host:, api_path:, content_type:, file_path:)
       @host = host
       @api_path = api_path
@@ -10,14 +13,22 @@ module SaturnCIWorkerAPI
     end
 
     def execute
-      Request.new(
-        host: @host,
-        endpoint: @api_path,
-        method: :post,
-        body: File.read(@file_path),
-        content_type: @content_type,
-        headers: { "X-Filename" => File.basename(@file_path) }
-      ).execute
+      uri = URI(url)
+      request = Net::HTTP::Post.new(uri)
+      request["Content-Type"] = @content_type
+      request["X-Filename"] = File.basename(@file_path)
+      request.basic_auth(ENV["TEST_RUNNER_ID"], ENV["TEST_RUNNER_ACCESS_TOKEN"])
+      request.body = File.read(@file_path)
+
+      Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+        http.request(request)
+      end
+    end
+
+    private
+
+    def url
+      "#{@host}#{API_BASE_PATH}/#{@api_path}"
     end
   end
 end
